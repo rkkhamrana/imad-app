6c2f876dc7b8fa16f182a0dd1817a6d514c1c71f
rkkhamrana <30655343+rkkhamrana@users.noreply.github.com>
2017-08-05 18:43:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
+app.get('ui/article-one', function (req, res) {
   res.sendFile('article-one requested and will be served here');
 }); 
-app.get('/article-two', function (req, res) {
+app.get('ui/article-two', function (req, res) {
   res.sendFile('article-one requested and will be served here');
 });
-  app.get('/article-three', function (req, res) {
+  app.get('ui/article-three', function (req, res) {
  res.sendFile('article-one requested and will be served here');
   });
   
