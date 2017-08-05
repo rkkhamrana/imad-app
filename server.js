9935d4e1d913a1c4faaf894756c921debf9a4274
rkkhamrana <30655343+rkkhamrana@users.noreply.github.com>
2017-08-05 18:31:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.get('/article-one', function (req, res) {
   res.sendFile('article-one requested and will be served here');
 }); 
 app.get('/article-two', function (req, res) {
-  res.sendFile('article-two requested and will be served here');
+  res.sendFile('article-one requested and will be served here');
 });
   app.get('/article-three', function (req, res) {
- res.sendFile('article-three requested and will be served here');
+ res.sendFile('article-one requested and will be served here');
   });
   
 app.get('/ui/style.css', function (req, res) {
@@ -32,6 +32,6 @@ app.get('/ui/madi.png', function (req, res) {
 // Use 8080 only for local development if you already have apache running on 80
 
 var port = 80;
-app.listen(8080, function () {
+app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
