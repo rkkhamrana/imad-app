2a473a80d5ee1dda247c55370e476709a2f86f71
rkkhamrana <30655343+rkkhamrana@users.noreply.github.com>
2017-08-12 21:37:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+SELECT * FROM 'test';
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 }); 
