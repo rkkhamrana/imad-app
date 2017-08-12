00adc3a8c6f92776a9b243bd7568d05d06e83925
rkkhamrana <30655343+rkkhamrana@users.noreply.github.com>
2017-08-12 21:25:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/rk.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'rk.png'));
 });
 
 
