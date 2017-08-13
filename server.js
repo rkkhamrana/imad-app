bc45a235295bc502232788d3a708e031aa08f9a4
rkkhamrana <30655343+rkkhamrana@users.noreply.github.com>
2017-08-13 21:11:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,6 +96,12 @@ app.get('/test-db', function (req, res) {
     // make a select request
     // return a response with the results
 });
+
+var counter - 0;
+app.get('/counter', function (req, res) {
+  counter = counter +1  ;
+    res.send( counter.toString());
+});    
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 }); 
